8a02daf036e3727184dbb236565134bda387f541
Dima <dmaznichenko@gmail.com>
2026-08-24 21:25:36 +0200

    chore: release v1.0.1980

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mirocard2-v10";
+const CACHE = "mirocard2-v11";
 
 self.addEventListener("install", () => {
   // Stay in the waiting phase until the app explicitly applies the update.
